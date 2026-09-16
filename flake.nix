10369f8a6413f18edcc81d730f8947fe1a67074e
Lloyd <lloyd.williams1062@gmail.com>
2026-09-16 15:23:45 +1000

    🙃

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "OxCaml package set";
+
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    oxcaml-opam-repository = {
+      url = "github:oxcaml/opam-repository/bb4555262936283daf5cbc82423509d4e7069b15";
+      flake = false;
+    };
+
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    opam-nix,
+    flake-utils,
+    oxcaml-opam-repository,
+    opam-repository,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        on = opam-nix.lib.${system};
+
+        scope =
+          on.buildOpamProject' {
+            repos = [
+              oxcaml-opam-repository
+              opam-repository
+            ];
+          } "oxcaml"
+          ./. {
+            ocaml-variants = "5.2.0+ox";
+          };
+      in {
+        legacyPackages = scope;
+      }
+    );
+}
